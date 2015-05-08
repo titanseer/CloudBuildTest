5adb9838a92da62f14851bb92cb7b0c451c34551
titanseer <titanseer1987@hotmail.com>
2015-05-08 16:08:08 -0700

    Rotate Cube

---
+++ Assets/Assets_Shawn/myScript/Rotate.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//int i=0;
+	//	i = Random.Range(-1,1);
+		this.gameObject.transform.Rotate(Random.Range(-1,1),Random.Range(-1,1),Random.Range(-1,1));
+
+	}
+}
